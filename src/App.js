486ead71d6ccbc936988db111736ff681370567f
Musili Brian <musilibrian07@gmail.com>
2022-02-02 10:12:23 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Card from './components/Card';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+  <Main/>
+  <Card/>
+    </div>
+  );
+}
+
+export default App;
